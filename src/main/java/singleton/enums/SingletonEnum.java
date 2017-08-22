d1446660f51474e16f1d3a856418e81337e6348d
yunqiangdi <yunqiangdi@hengtiansoft.com>
2017-08-22 17:22:08 +0800

    update

---
+++ src/main/java/singleton/enums/SingletonEnum.java
@@ -0,0 +1,14 @@
+package singleton.enums;
+
+/**
+ * Description:枚举型单例
+ *
+ * @author yunqiangdi
+ * @version 1.0
+ * @since 2017-08-22 5:12 PM
+ */
+public enum SingletonEnum {
+
+    INSTANCE;
+
+}
---
+++ src/main/java/singleton/enums/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * @author yunqiangdi
+ * @version 1.0
+ * @since 2017-08-22 5:11 PM
+ */
+package singleton.enums;
